fa600138cf5c4767012921e23e1556d2796d96ee
DillonPittman <pittmanwdillon@gmail.com>
2018-11-27 20:27:12 -0600

    added amazon linked title and imdb linked rating

--- assets/javascript/movie.js
+++ assets/javascript/movie.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
 
       var movieDiv = $("<div class='card col-lg-4'>");
 
-      var title = $("<h1 class='title'>").text(data.Title);
+      var title = $("<a href='https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords="+data.Title+"' class='title'>").text(data.Title);
       movieDiv.append(title)
 
       var pOne = $("<p>").text("Rating: " + data.Rated);
@@ -42,16 +42,16 @@ $(document).ready(function () {
       var pSix = $("<p>").text("Metascore: "+ data.Metascore);
       movieDiv.append(pSix);
 
-      var pSeven = $("<p>").text("IMDB Rating: "+ data.imdbRating);
+      var pSeven = $("<a href='https://www.imdb.com/find?ref_=nv_sr_fn&q="+data.Title+"&s=all'>").text("IMDB Rating: "+ data.imdbRating);
       movieDiv.append(pSeven);
 
       var pEight = $("<p>").text("Runtime: "+ data.Runtime);
       movieDiv.append(pEight);
 
       var pNine = $("<p>").text("Writer: " + data.Writer);
-      movieDiv.append(pEight);
+      movieDiv.append(pNine);
 
-      var image = $("<img>").attr("src", data.Poster);
+      var image = $("<img class='img-fluid'>").attr("src", data.Poster);
       movieDiv.append(image);
 
       $("#movies-here").prepend(movieDiv);
