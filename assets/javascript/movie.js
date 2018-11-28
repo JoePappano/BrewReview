4f885750f09b93b0601cfff8100ee2363915ce62
DillonPittman <pittmanwdillon@gmail.com>
2018-11-27 21:30:41 -0600

    chat started

--- assets/javascript/movie.js
+++ assets/javascript/movie.js
@@ -11,6 +11,7 @@
 $(document).ready(function () {
    
   $("#submitMovie").on("click", function () {
+    
     event.preventDefault();
     var movie = $("#movie").val().trim();
     var queryURL = "https://www.omdbapi.com/?t=" + movie + "&plot=short&apikey=de45c0f0";
@@ -24,31 +25,31 @@ $(document).ready(function () {
       var title = $("<a href='https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords="+data.Title+"' class='title'>").text(data.Title);
       movieDiv.append(title)
 
-      var pOne = $("<p>").text("Rating: " + data.Rated);
+      var pOne = $("<div class='text'>").text("Rating: " + data.Rated);
       movieDiv.append(pOne);
 
-      var pTwo = $("<p>").text("Released: " + data.Released);
+      var pTwo = $("<div class='text'>").text("Released: " + data.Released);
       movieDiv.append(pTwo);
 
-      var pThree = $("<p>").text("Plot: " + data.Plot);
+      var pThree = $("<div class='text'>").text("Plot: " + data.Plot);
       movieDiv.append(pThree);
 
-      var pFour = $("<p>").text("Actors: "+ data.Actors);
+      var pFour = $("<div class='text'>").text("Actors: "+ data.Actors);
       movieDiv.append(pFour);
 
-      var pFive = $("<p>").text("Awards: "+ data.Awards);
+      var pFive = $("<div class='text'>").text("Awards: "+ data.Awards);
       movieDiv.append(pFive);
 
-      var pSix = $("<p>").text("Metascore: "+ data.Metascore);
+      var pSix = $("<div class='text'>").text("Metascore: "+ data.Metascore);
       movieDiv.append(pSix);
 
       var pSeven = $("<a href='https://www.imdb.com/find?ref_=nv_sr_fn&q="+data.Title+"&s=all'>").text("IMDB Rating: "+ data.imdbRating);
       movieDiv.append(pSeven);
 
-      var pEight = $("<p>").text("Runtime: "+ data.Runtime);
+      var pEight = $("<div class='text'>").text("Runtime: "+ data.Runtime);
       movieDiv.append(pEight);
 
-      var pNine = $("<p>").text("Writer: " + data.Writer);
+      var pNine = $("<div class='text'>").text("Writer: " + data.Writer);
       movieDiv.append(pNine);
 
       var image = $("<img class='img-fluid'>").attr("src", data.Poster);
@@ -56,6 +57,7 @@ $(document).ready(function () {
 
       $("#movies-here").prepend(movieDiv);
 
+      
     });
   });
 });
