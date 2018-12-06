51d872c7a67d855f9fc08c72850ca882074a989f
HFMarley <hankfoxmarley@gmail.com>
2018-12-05 23:01:41 -0600

    added template literals to js files and added styling to the dynamically created elements

--- assets/javascript/movie.js
+++ assets/javascript/movie.js
@@ -19,43 +19,31 @@ $(document).ready(function () {
       url: queryURL,
       method: "GET"
     }).then(function(data) {
+      var movieHtml=`<div class="col-lg-4">
+      <div class="card movieCard">
+          <div class="card-header headText text-white bg-dark">                   
+              <a href="https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&amp;field-keywords=Hulk" class="title text-info">
+                  <h5 class="card-title">${data.Title}</h5>
+              </a>
+          </div>
+          <div class="card-body">
+              <p class="card-text"><strong>Rating:</strong> ${data.Rated}</p>
+              <p class="card-text"><strong>Released:</strong> ${data.Released}</p>
+              <p class="card-text"><strong>Plot:</strong> ${data.Plot}</p>
+              <p class="card-text"><strong>Actors:</strong> ${data.Actors}</p>
+              <p class="card-text"><strong>Awards:</strong> ${data.Awards}</p>
+              <p class="card-text"><strong>Metascore:</strong> ${data.Metascore}</p>
+              <a href="https://www.imdb.com/find?ref_=nv_sr_fn&amp;q=" hulk'&s="all'">
+                  <p class="card-text"><strong>IMDB Rating:</strong> ${data.imdbRating}</p>
+              </a>
+              <p class="card-text"><strong>Runtime:</strong> ${data.Runtime}</p>
+              <p class="card-text"><strong>Writer:</strong> ${data.Writer}</p>
+          </div>
+          <img class="card-img-bottom" src=${data.Poster}>
+      </div>
+  </div>`
 
-      var movieDiv = $("<div class='card col-lg-4 border border-dark'>");
-
-      var title = $("<a href='https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords="+data.Title+"' class='title'>").text(data.Title);
-      movieDiv.append(title)
-
-      var pOne = $("<div class='text'>").text("Rating: " + data.Rated);
-      movieDiv.append(pOne);
-
-      var pTwo = $("<div class='text'>").text("Released: " + data.Released);
-      movieDiv.append(pTwo);
-
-      var pThree = $("<div class='text'>").text("Plot: " + data.Plot);
-      movieDiv.append(pThree);
-
-      var pFour = $("<div class='text'>").text("Actors: "+ data.Actors);
-      movieDiv.append(pFour);
-
-      var pFive = $("<div class='text'>").text("Awards: "+ data.Awards);
-      movieDiv.append(pFive);
-
-      var pSix = $("<div class='text'>").text("Metascore: "+ data.Metascore);
-      movieDiv.append(pSix);
-
-      var pSeven = $("<a href='https://www.imdb.com/find?ref_=nv_sr_fn&q="+data.Title+"&s=all'>").text("IMDB Rating: "+ data.imdbRating);
-      movieDiv.append(pSeven);
-
-      var pEight = $("<div class='text'>").text("Runtime: "+ data.Runtime);
-      movieDiv.append(pEight);
-
-      var pNine = $("<div class='text'>").text("Writer: " + data.Writer);
-      movieDiv.append(pNine);
-
-      var image = $("<img class='img-fluid'>").attr("src", data.Poster);
-      movieDiv.append(image);
-
-      $("#movies-here").prepend(movieDiv);
+      $("#movies-here").prepend(movieHtml);
       $("#movie").val("");
     });
   });
